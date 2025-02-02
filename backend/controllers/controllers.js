8311f63e3eec4c7d9651b57a4b9ea8dd5f2e6b54
Oscar Roni <oscar.gt.140.ro@gmail.com>
2025-02-02 14:42:35 -0600

    update and delete operations for actors

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -27,7 +27,33 @@ export const getActors = async (req, res) => {
 export const getActorById = async (req, res) => {
   try {
     const actor = await Actor.findById(req.params.id)
+
+    if (!actor) {
+      res.status(404).send('Actor not found')
+      return
+    }
+
     res.status(200).json(actor)
+  }
+  catch (error) {
+    res.status(500).json({ error: error })
+  }
+}
+
+export const updateActor = async (req, res) => {
+  try {
+    const actor = await Actor.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.status(200).json(actor)
+  }
+  catch (error) {
+    res.status(500).json({ error: error })
+  }
+}
+
+export const deleteActor = async (req, res) => {
+  try {
+    await Actor.findByIdAndDelete(req.params.id)
+    res.status(204).send()
   }
   catch (error) {
     res.status(500).json({ error: error })
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
-import { getHello, createActor, getActors, getActorById } from "../controllers/controllers.js";
+import { getHello, createActor, getActors, getActorById, deleteActor, updateActor } from "../controllers/controllers.js";
 
 const router = Router();
 
 router.get('/', getHello);
 
-// to create actors
 router.post('/actors', createActor);
 
 router.get('/actors', getActors);
 
 router.get('/actors/:id', getActorById);
+
+router.put('/actors/:id', updateActor);
+
+router.delete('/actors/:id', deleteActor);
 
 export default router;
